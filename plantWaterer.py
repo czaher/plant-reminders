a1eaf56321bfbb15ccae161e622008cdccbba3eb
zahercj <corey@refractlabs.com>
2021-03-13 23:24:47 -0500

    added messaging functionality with twilio

--- plantWaterer.py
+++ plantWaterer.py
@@ -1,2 +1,22 @@
-print("hello world")
-print("is this thing on?")
+import os
+from twilio.rest import Client
+from dotenv import load_dotenv
+
+load_dotenv()
+
+def sendMessage(smsMessage):
+    account_sid = os.environ['TWILIO_ACCOUNT_SID']
+    auth_token = os.environ['TWILIO_AUTH_TOKEN']
+    client = Client(account_sid, auth_token)
+
+    message = client.messages \
+        .create(
+            body=smsMessage,
+            from_=os.environ['TWILIO_NUMBER'],
+            to = os.environ['CONTACT_NUMBER'],
+        )
+
+messageToSend = 'water your 🪴s'
+sendMessage(messageToSend)
+
+
